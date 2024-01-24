d09dc32ebbdf5a84cec01f560ccc8e1da6620a46
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-24 19:23:15 +0530

    routes added to index.js file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const connnectDB = require('../db')
+const craftRoute = require('./routes/CraftRoutes');
+const responseRoute = require('./routes/ResponseRoutes');
 const port = 8080;
 
 app.get('/', (req, res) => {
     res.send("started")
 });
+
+app.use('/craft', craftRoute)
+app.use('/response', responseRoute)
 
 app.lisetn(port, ()=>{
     console.log(`Listening on port ${port}`);
