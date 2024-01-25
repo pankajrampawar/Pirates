962165a9fe8e2a7829195fd260ff54826626749e
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-25 17:56:11 +0530

    updated page.js file

--- client/app/page.js
+++ client/app/page.js
@@ -2,10 +2,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="h-full w-full bg-black text-white justify-center items-center">
+    <main className="h-full w-full bg-black text-white flex justify-center items-center">
       <header>
         NEXUS
       </header>
+
+      <section>
+        <article>
+         <p>
+          This website was directly crafted by GODS*, it is a doorway to 
+          <span> unfiltered Community</span>
+         </p>
+          <p>
+            enter at your own risk
+          </p>
+        </article>
+      </section>
+
+      <section>
+        <div>
+          <button>
+            Login
+          </button>
+          <button>
+            Signup
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
