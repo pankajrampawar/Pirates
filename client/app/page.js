82bfab4c4c220f365b86f467f4d58d189c46e744
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-26 18:11:36 +0530

    added link to landing page for login and signup

--- client/app/page.js
+++ client/app/page.js
@@ -1,18 +1,7 @@
-'use client'
-
-import Image from "next/image";
 import Link from "next/link";
 import { keaniaOne, happyMonkey } from "./fonts";
 
 export default function Home() {
-
-  const handleLogin = () => {
-    console.log("Login clicked")
-  };
-
-  const handleSignup = () => {
-    console.log("signup clicked")
-  };
 
   return (
     <main className="h-full w-full bg-black text-white flex items-center flex-col gap-9 pt-20 px-7">
@@ -38,18 +27,18 @@ export default function Home() {
           <Link href='/login'>
             <button 
               className="bg-white text-black p-3 w-[130px] rounded-3xl"
-              onClick={handleLogin}
             >
               Login
             </button>
           </Link>
           
-          <button 
-            className="bg-white text-black p-3 w-[130px] rounded-3xl"
-            onClick={handleSignup}
-          >
-            Signup
-          </button>
+          <Link href="./signup/page1">
+            <button 
+              className="bg-white text-black p-3 w-[130px] rounded-3xl"
+            >
+              Signup
+            </button>
+          </Link>
         </div>
       </section>
     </main>
