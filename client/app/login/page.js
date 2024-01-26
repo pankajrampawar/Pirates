2249d5fe67cecbfbbeab5f10aba1b1302c92c740
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-26 17:20:34 +0530

    login page added

---
+++ client/app/login/page.js
@@ -0,0 +1,44 @@
+'use client'
+
+import React from 'react';
+import { keaniaOne, happyMonkey } from '../fonts';
+import Image from 'next/image';
+
+export default function Login() {
+    return (
+        <div className='h-full px-10 pb-20 flex flex-col py-20 text-center items-center justify-center gap-40'>
+           <header className={`text-[33px] ${keaniaOne.className} tracking-wider`}>
+              Nexus
+            </header>
+
+            <section className='flex justify-center border-b border-gray-400 pb-2'>
+                <Image
+                    src="./user.svg"
+                    alt='user'
+                    width={29}
+                    height={29}
+                    className='invert'
+                />
+                <input
+                    placeholder='username'
+                    className={`bg-black ${happyMonkey.className} text-xl pl-5`}
+                />
+            </section>
+
+            <section className='flex justify-center'>
+                <button
+                     className={`${happyMonkey.className} bg-white text-black text-3xl flex flex-col justify-center items-center h-[90px] w-[120px] rounded-[50px]`}
+                >
+                    <Image 
+                        src="./next.svg"
+                        alt='next'
+                        height={40}
+                        width={40}
+                        className='-mb-2'
+                    />
+                    <p>Next</p>
+                </button>
+            </section>
+        </div>
+    )
+}
--- client/app/page.js
+++ client/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from "next/image";
+import Link from "next/link";
 import { keaniaOne, happyMonkey } from "./fonts";
 
 export default function Home() {
@@ -19,11 +20,12 @@ export default function Home() {
         Nexus
       </header>
 
-      <section className={`${happyMonkey.className} tracking-wider text-center text-[21px]`}>
-        <article className="flex flex-col gap-6">
+      <section className={`${happyMonkey.className} tracking-wider text-center text-[20px]`}>
+        <article className="flex flex-col gap-7">
          <p>
           This website was directly crafted by GODS*, it is a doorway to 
-          <span className="underline underline-offset-8 decoration-red-600"> unfiltered Community</span>
+          <span> </span> 
+          <span className="underline underline-offset-8 decoration-red-600">unfiltered Community</span>
          </p>
           <p>
             Enter at your own risk...
@@ -33,14 +35,17 @@ export default function Home() {
 
       <section className={`${keaniaOne.className} text-xl mt-4`}>
         <div className="flex gap-4">
+          <Link href='/login'>
+            <button 
+              className="bg-white text-black p-3 w-[130px] rounded-3xl"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </Link>
+          
           <button 
-            className="bg-white text-black p-3 w-[145px] rounded-3xl"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-          <button 
-            className="bg-white text-black p-3 w-[145px] rounded-3xl"
+            className="bg-white text-black p-3 w-[130px] rounded-3xl"
             onClick={handleSignup}
           >
             Signup
