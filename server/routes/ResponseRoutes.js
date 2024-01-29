06ec82b85a576dc5cb32c1a697fc18c7fc491663
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-29 15:30:47 +0530

    added middleware to repsonse route

--- server/routes/ResponseRoutes.js
+++ server/routes/ResponseRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const responseController = require('../controllers/responseController');
+const authMiddleware = require('../middleware/authMiddleware')
 
-router.get('/getResponses', responseController.getAllResponsesForCraft);
-router.post('/addResponse', responseController.addResponse)
+router.get('/getResponses', authMiddleware, responseController.getAllResponsesForCraft);
+router.post('/addResponse',authMiddleware, responseController.addResponse)
 
 module.exports = router;
