70ac1125abc084e623e721c8423c7e00c6074ad2
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-29 15:25:00 +0530

    added middleware logic

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const { verifyToken } = require("../utils/tokenUtils");
+require('dotenv').config();
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        console.log("entering auth middleware")
+        const accessToken = req.cookies.accessToken;
+        console.log(accessToken);
+
+        if (!accessToken) {
+            return res.status(404).json({ message: "access token not found" });
+        }
+
+        const decoded = verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+        req.userId = decoded;
+
+        // Call the next middleware or route handler
+        next();
+    } catch (error) {
+        return res.status(500).json({ message: "internal server error (auth middleware)", error });
+    }
+};
+
+module.exports = authMiddleware;
