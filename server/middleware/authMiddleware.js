adb336db03a30a4ebdae5ab1489d8d69948c22f2
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-31 18:59:51 +0530

    changed middleware routing to include refresh token

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,23 +1,45 @@
-const { verifyToken } = require("../utils/tokenUtils");
+const { verifyToken, generateRefreshToken, generateAccessToken } = require("../utils/tokenUtils");
 require('dotenv').config();
 
 const authMiddleware = async (req, res, next) => {
-    try {
-        const accessToken = req.cookies.accessToken;
 
-        if (!accessToken) {
-            return res.status(404).json({ message: "access token not found" });
-        }
+    const { accessToken, refreshToken } = req.cookies;
 
-        const decoded = verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET);
-
-        req.userId = decoded;
-
-        // Call the next middleware or route handler
-        next();
-    } catch (error) {
-        return res.status(500).json({ message: "internal server error (auth middleware)", error });
+    if (!accessToken || !refreshToken) {
+        return res.status(401).json({ message: "unathorized" });
     }
+
+    console.log("decoding");
+    const decoded = verifyToken(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+    if (decoded) {
+        req.userId = decoded.userId;
+        return next();
+    }
+
+    console.log("access token invalid");
+    const decodedRefreshToken = verifyToken(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+    if (!decodedRefreshToken) {
+        console.log("refresh token not found");
+        return res.status(401).json({ message: "unauthorized, plz login in again."});
+    }
+
+    const newAccessToken = generateAccessToken(decodedRefreshToken.userId);
+
+    if (!newAccessToken) {
+        return res.status(500).json({ message: "unable to genrated access token, please login again."});
+    }
+    
+    req.userId = decodedRefreshToken.userId;
+
+    res.cookie('accessToken', newAccessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict'
+    });
+
+    return next();
 };
 
 module.exports = authMiddleware;
