a05814f891ea2fbc1506b8baacb04b0258f0dfb3
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-31 23:01:55 +0530

    page 1 layout changed

--- client/app/signup/page1/page.js
+++ client/app/signup/page1/page.js
@@ -13,21 +13,8 @@ export default function PageOne() {
             </header>
 
             <section className='flex flex-col gap-14'>
-                <div className='flex gap-2 border-b border-gray-400 pb-2'>
-                    <Image
-                        src="../user.svg"
-                        alt='user'
-                        height={29}
-                        width={29}
-                        className='invert' 
-                    />
-                    <input 
-                        placeholder='username'
-                        className={`bg-black focus:outline-none ${happyMonkey.className} text-xl`}
-                    />
-                </div>
-
-                <div className='flex gap-2 border-b border-gray-400 pb-2'>
+                
+                <div className='flex gap-2 border-b border-gray-400 pb-2 min-w-[280px]'>
                     <Image
                         src="../college.svg"
                         alt='college'
@@ -39,10 +26,50 @@ export default function PageOne() {
                         className={`bg-black w-full focus:outline-none ${happyMonkey.className} tracking-widest text-xl `}
                         placeholder="college"
                     >
-                        <option value="" style={{color: '#718096'}} disabled selected>Select College</option>
+                        <option value="" style={{color: '#718096'}} disabled selected>College</option>
                         <option value="VESIT">VESIT</option>
                         <option value="SPIT">SPIT</option>
                         <option value="NMIMS">NMIMS</option>
+                    </select>
+                </div>
+
+                <div className='flex gap-2 border-b border-gray-400 pb-2'>
+                    <Image
+                        src="../year.svg"
+                        alt='user'
+                        height={29}
+                        width={29}
+                        className='invert' 
+                    />
+                    <select
+                        className={`bg-black w-full focus:outline-none ${happyMonkey.className} tracking-widest text-xl `}
+                        placeholder="college"
+                    >
+                        <option value="" style={{color: '#718096'}} disabled selected>Year</option>
+                        <option value="1">1st</option>
+                        <option value="2">2nd</option>
+                        <option value="3">3rd</option>
+                        <option value="4">4th</option>
+                    </select>
+                </div>
+
+                <div className='flex gap-2 border-b border-gray-400 pb-2'>
+                    <Image
+                        src="../branch.svg"
+                        alt='college'
+                        height={29}
+                        width={29}
+                        className='invert'
+                    />
+                    <select
+                        className={`bg-black w-full focus:outline-none ${happyMonkey.className} tracking-widest text-xl `}
+                        placeholder="college"
+                    >
+                        <option value="" style={{color: '#718096'}} disabled selected>Branch</option>
+                        <option value="CMPN">CMPN</option>
+                        <option value="EXTC">EXTC</option>
+                        <option value="AIDS">AIDS</option>
+                        <option value="ECS">ECS</option>
                     </select>
                 </div>
             </section>
