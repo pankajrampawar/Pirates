7f756770e6c1b0155c057c97a8aa1f13e20ca07a
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-31 22:51:09 +0530

    changed route name to singup

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post("/createNewUser", userController.createNewUser);
+router.post("/signup", userController.createNewUser);
 router.post('/login', userController.userLogin)
 
 module.exports = router
