1f082da172acf4e6b7e7e03b4a817f3de05938c0
pankaj ram pawar <pankajpawars123@gmail.com>
2024-02-02 18:47:52 +0530

    completed post message component

---
+++ client/app/ui/postAMessage.js
@@ -0,0 +1,63 @@
+'use client'
+
+
+import React, { useState, useEfffect} from 'react';
+import { happyMonkey } from '../fonts';
+import { postADrop } from '../actions';
+
+export default function PostDrop(props) {
+
+    const [drop, setDrop] = useState({
+        craftToAdd: ''
+    });
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+
+        setDrop((prev) => ({
+            ...prev,
+            [id] : value
+        }))
+    }
+
+    const handleAddDrop = async () => {
+        if (!drop.craftToAdd) {
+            alert('type something to drop');
+            
+            return;
+        }
+
+        const response = await postADrop(drop);
+
+        if (response) {
+            alert('drop sent');
+            props.togglePostCardVisible();
+        }
+    }
+
+    return (
+        <div className='bg-surface p-4 rounded-lg flex items-end flex-col gap-4 min-w-[270px] relative'>
+                <div className='w-full'>
+                        <textarea
+                            className='bg-surface focus:outline-none pb-2 border-b border-gray-400 w-full h-20'
+                            placeholder='type a drop..'
+                            id='craftToAdd'
+                            value={drop.craftToAdd}
+                            onChange={handleChange}
+                        />
+                </div>
+                <div className=''>
+                    <button 
+                        className={`bg-white text-black font-bold ${happyMonkey.className} p-2 rounded-md h-fit`}
+                        onClick={handleAddDrop}    
+                    >
+                        Drop
+                    </button>
+                </div>
+
+                <p className={`absolute text-base -bottom-20 left-2 ${happyMonkey.className}`}>
+                    your drops are completely anonymous, even gods can't trace them..
+                </p>
+        </div>
+    )
+}
