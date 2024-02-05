b5fef5d83bb1f2ba43d80aeda667f356eb8e689e
pankaj ram pawar <pankajpawars123@gmail.com>
2024-02-06 03:03:01 +0530

    reply card changed

--- client/app/ui/replyCard.js
+++ client/app/ui/replyCard.js
@@ -5,14 +5,14 @@ import React, { useState, useEffect } from 'react';
 export default function ReplyCard(props) {
 
     return (
-        <div className='bg-surface flex flex-col rounded-l-2xl p-4 pb-1 pt-3'>
+        <div className='bg-surface flex flex-col rounded-l-2xl p-4 pb-2 pt-3'>
             <section className='pb-3 border-b border-gray-400'>
                 <p>
                     {props.content}
                 </p>
             </section>
             <section className='flex gap-3 justify-center pt-3'>
-            <div className='flex gap-1 justify-center items-center'>
+            {/* <div className='flex gap-1 justify-center items-center'>
                         <span>
                             🫦
                         </span>
@@ -35,7 +35,7 @@ export default function ReplyCard(props) {
                         <span className='text-sm'>
                             30
                         </span>
-                    </div>
+                    </div> */}
             </section>
         </div>
     )
