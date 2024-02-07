841771da9ce0865b9d0330fe9c9fb0f7abac98b3
pankaj ram pawar <pankajpawars123@gmail.com>
2024-02-07 18:43:05 +0530

    added metadata and description

--- client/app/layout.js
+++ client/app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Whiseve",
-  description: "whiseve",
+  description: "whiseve.com is a website that facilitates college communites with anonymous messaging and interactive tools",
 };
 
 export default function RootLayout({ children }) {
