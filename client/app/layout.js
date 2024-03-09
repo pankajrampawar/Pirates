ed458812511404711acb4520e40ccae5b5c078b4
pankaj ram pawar <pankajpawars123@gmail.com>
2024-03-09 13:40:26 +0530

    analytics added

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`h-full ${inter.className} bg-black text-white`}>
         {children}
+        <Analytics/>
         <SpeedInsights/>
         </body>
     </html>
