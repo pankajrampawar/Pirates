d05484c110587ccdfd3eb4d43234413172c661dd
pankaj ram pawar <pankajpawars123@gmail.com>
2024-03-10 10:05:45 +0530

    font optimized

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,18 +1,29 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Keania_One, Happy_Monkey } from "next/font/google";
+
+export const keaniaOne = Keania_One({
+    subsets: ['latin'],
+    weight: ['400']
+})
+
+export const happyMonkey = Happy_Monkey({
+    subsets: ['latin'],
+    weight: ['400']
+})
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Whiseve",
-  description: "whiseve.com is a website that facilitates college communites with anonymous messaging and interactive tools",
+  description: " whiseve whiseve.com is a website that facilitates college communites with anonymous messaging and interactive tools",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`h-full ${inter.className} bg-black text-white`}>
+      <body className={`h-full  bg-black text-white`}>
         {children}
         <SpeedInsights/>
         </body>
